9ed31eb00d43da9ef4766baad25028dc0120e2db
GpsUkr <oleggpscom@gmail.com>
2023-01-05 20:53:21 +0200

    work-5

---
+++ js/slider.js
@@ -0,0 +1,15 @@
+$(".customer_slider").slick({
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 10000,
+    pausOnDotHover: true,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    variableWidth: false,
+    slidesToScroll: 1
+  });
+  
